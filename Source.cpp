73facfd36d3b013d8764ea33781378cfcc77ffaf
ennur <58693641+ennur@users.noreply.github.com>
2020-01-16 17:17:05 +0200

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Islem.h"
+#include "Kontrol.h"
+#include "Musteri.h"
+#include "Urun.h"
+#include "Tarih.h"
+#include <iomanip>
+using namespace std;
+
+	
+int main()
+{   
+	srand(time(0));
+	
+	Kontrol kont;
+	kont.anaMenuCiz();
+
+	return 0;
+}
